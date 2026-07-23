0818d57f4af7507c74b86795b85f742e658a9318
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-07-23 11:34:32 +0530

    Time: 3 ms (90.38%), Space: 20.6 MB (13.83%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            # If pattern is exhausted
+            if j == len(p):
+                return i == len(s)
+
+            # Check if current characters match
+            first_match = (
+                i < len(s) and
+                (p[j] == s[i] or p[j] == ".")
+            )
+
+            # If next character is '*'
+            if j + 1 < len(p) and p[j + 1] == "*":
+                memo[(i, j)] = (
+                    dp(i, j + 2) or
+                    (first_match and dp(i + 1, j))
+                )
+            else:
+                memo[(i, j)] = (
+                    first_match and dp(i + 1, j + 1)
+                )
+
+            return memo[(i, j)]
+
+        return dp(0,0)
+        
