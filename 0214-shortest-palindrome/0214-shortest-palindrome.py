462dbc0904778c16b7533f3f946facd322f79cca
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-07-24 09:15:16 +0530

    Time: 43 ms (79.37%), Space: 23.8 MB (16.69%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,58 +1,30 @@
-class Manacher:
-    def __init__(self, s):
-        # Modified string
-        self.ms = "@"
-        for ch in s:
-            self.ms += "#" + ch
-        self.ms += "#$"
-
-        self.p = [0] * len(self.ms)
-        self.run_manacher()
-
-    def run_manacher(self):
-        n = len(self.ms)
-        left = right = 0
-
-        for i in range(1, n - 1):
-
-            if i < right:
-                self.p[i] = min(right - i, self.p[left + right - i])
-
-            while self.ms[i + self.p[i] + 1] == self.ms[i - self.p[i] - 1]:
-                self.p[i] += 1
-
-            if i + self.p[i] > right:
-                left = i - self.p[i]
-                right = i + self.p[i]
-
-    def getLongest(self, center, odd):
-        pos = 2 * center + 2 + (0 if odd else 1)
-        return self.p[pos]
-
-    def check(self, l, r):
-        length = r - l + 1
-        longest = self.getLongest((l + r) // 2, length % 2 == 1)
-        return longest >= length
-
-
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
 
-        n = len(s)
+        rev = s[::-1]
 
-        if n <= 1:
-            return s
+        temp = s + "#" + rev
 
-        m = Manacher(s)
+        # Build LPS
+        lps = [0] * len(temp)
 
-        # Find longest palindromic prefix
-        longestPrefix = 0
+        length = 0
+        i = 1
 
-        for i in range(n - 1, -1, -1):
-            if m.check(0, i):
-                longestPrefix = i + 1
-                break
+        while i < len(temp):
 
-        suffix = s[longestPrefix:]
+            if temp[i] == temp[length]:
+                length += 1
+                lps[i] = length
+                i += 1
 
-        return suffix[::-1] + s
+            elif length != 0:
+                length = lps[length - 1]
+
+            else:
+                lps[i] = 0
+                i += 1
+
+        longest = lps[-1]
+
+        return rev[:len(s) - longest] + s
